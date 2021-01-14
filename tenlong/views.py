3069dc5f5362627c0f17f526ebfa1caa559bb7ab
osmuntsun <osmuntsun900606@gmail.com>
2021-01-14 13:44:11 +0800

    完全

---
+++ tenlong/views.py
@@ -0,0 +1,150 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from bs4 import BeautifulSoup
+from django.forms.models import model_to_dict
+from users.models import Books
+import requests
+import json
+
+
+# Create your views here.
+
+def index(request):
+    user_id = request.user.id
+
+    return render(request, 'tenlong/index.html', {
+        'navbar': get_navbar(),
+        'books_detail': get_books_detail(user_id)
+    })
+
+
+def special(request, special_id):
+    return render(request, 'tenlong/books.html', {
+        'navbar': get_navbar(),
+        'special_id': special_id
+    })
+
+
+def keyword(request):
+    return render(request, 'tenlong/keyword.html', {
+        'navbar': get_navbar(),
+        'keyword': request.POST['keyword']
+    })
+
+
+def get_keyword_books(request, book_name, book_page):
+    req_text = requests.get(f'https://www.tenlong.com.tw/search', {
+        'keyword': book_name,
+        'page': book_page
+    }).text
+    books = []
+    book_soup = BeautifulSoup(req_text, 'html.parser').find('div', class_='search-result-list').find('ul').find_all(
+        'li', class_=lambda x: x != 'promo')
+
+    for book in book_soup:
+        try:
+            book_link = book.find('a', class_='cover').get('href')
+            book_img = book.find('a', class_='cover').find('img').get('src')
+            book_info = '/'.join([x.text for x in
+                                  book.find('div', class_='book-data').find('ul', class_='item-info').find('li',
+                                                                                                           class_='basic').find_all(
+                                      'span')])
+            book_status = '/'.join([x.text.strip() for x in
+                                    book.find('div', class_='book-data').find('ul', class_='item-info').find('li',
+                                                                                                             class_='pricing').find_all(
+                                        'span')])
+
+            books.append({
+                'link': f'https://www.tenlong.com.tw/{book_link}',
+                'img': book_img,
+                'info': book_info,
+                'status': book_status
+            })
+        except:
+            pass
+
+    return HttpResponse(json.dumps(books))
+
+
+def publisher(request, publisher_id):
+    return render(request, 'tenlong/books.html', {
+        'navbar': get_navbar(),
+        'publishers_id': publisher_id
+    })
+
+
+def publishers(request):
+    req_text = requests.get('https://www.tenlong.com.tw//publishers').text
+    publisher_soup = BeautifulSoup(req_text, 'html.parser').find('ul', class_='category-list--full').findAll('a')
+    publisher_data = [{
+        'name': item.text,
+        'link': item.get('href')
+    } for item in publisher_soup]
+
+    return render(request, 'tenlong/publishers.html', {
+        'navbar': get_navbar(),
+        'publishers': publisher_data
+    })
+
+
+def get_navbar():
+    req_text = requests.get('https://www.tenlong.com.tw/').text
+    sidebar_data = []
+    sidebars = BeautifulSoup(req_text, 'html.parser').findAll('div', class_='sidebox')
+
+    for sidebar in sidebars:
+        sidebar_text = sidebar.find('h2').text
+        sidebar_not_hidden = ['特價書籍']
+
+        if sidebar_text in sidebar_not_hidden:
+            sidebar_data.append({
+                'name': sidebar.find('h2').text,
+                'link_dict': [{
+                    'link': str(link.find('a').get('href')),
+                    'text': link.find('a').text
+                } for link in sidebar.find('ul').find_all('li')]
+            })
+
+    return sidebar_data
+
+
+def get_books(request, book_species, book_id, book_page):
+    req_text = requests.get(f'https://www.tenlong.com.tw/{book_species}/{book_id}', {
+        'page': book_page
+    }).text
+    book_soup = BeautifulSoup(req_text, 'html.parser').find('div', class_='list-wrapper').find('ul').findAll('li',
+                                                                                                             class_='single-book')
+    book_data = []
+
+    for book_ele in book_soup:
+        book_title = book_ele.find('strong', class_='title')
+        book_price = list(
+            filter(lambda x: x != '', book_ele.find('div', class_='pricing').text.strip().split(' ')))
+        book_data.append({
+            'name': book_title.find('a').text,
+            'img_link': book_ele.find('a', class_='cover').find('img').get('src'),
+            'price': book_price[1 if len(book_price) > 1 else 0],
+            'link': 'https://www.tenlong.com.tw' + book_title.find('a').get('href'),
+            'product_id': list(filter(None, book_title.find('a').get('href').replace('?', '/').split('/')))[1]
+        })
+
+    return HttpResponse(json.dumps(book_data))
+
+
+def get_books_detail(user_id):
+    products_url = [f'https://www.tenlong.com.tw/products/{data["product_id"]}' for data in
+                    [model_to_dict(data) for data in Books.objects.filter(user_id=user_id)]]
+    products = []
+
+    for url in products_url:
+        req_text = requests.get(url).text
+        products_main = BeautifulSoup(req_text, 'html.parser').find('div', class_='item-info')
+        products.append({
+            'img': products_main.find('div', class_='img-wrapper').find('a').find('picture').find('img').get('src'),
+            'detail': [
+                [x.find('span', class_='info-title').text.strip(), x.find('span', class_='info-content').text.strip()]
+                for x in products_main.find('ul', class_='item-sub-info').findAll('li')],
+            'buy_link': url
+        })
+
+    return products
